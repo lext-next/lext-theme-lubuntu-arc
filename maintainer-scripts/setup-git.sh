53c1348f928bc87f32cddcfc21d40bdd54a228cc
Lext Next <nextlxqt@gmail.com>
2025-09-18 10:21:10 +0300

    Corrected transport

--- maintainer-scripts/setup-git.sh
+++ maintainer-scripts/setup-git.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-git remote set-url origin git@github.com:lext-next/lext-artwork-lubuntu.git
+git remote set-url origin git@github.com:lext-next/lext-theme-lubuntu-arc.git
 git config user.name "Lext Next"
